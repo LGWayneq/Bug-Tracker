25b1d39be1f1843b13a841cc2035f77f202c46ad
LGWayneq <g_wayneliau@yahoo.com.sg>
2022-01-24 00:16:14 +0800

    update react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import Home from "./components/Home";
+import React, {useState} from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+function Login() {
+    const username = useFormInput('');
+    const password = useFormInput('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleLogin = () => {
+        
+    }
+
+    return (
+        <div>
+            Login<br/><br/>
+            <div>
+                Username<br/>
+                <input type="text" {...username} autoComplete="new-password"/>
+            </div>
+            <div>
+                Password<br/>
+                <input type="password" {...password} autoComplete="new-password"/>
+            </div>
+            <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading}/><br/>
+        </div>
+    );
 }
+
+const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChange = e => {
+        setValue(e.target.value);
+    }
+
+    return {
+        value,
+        onChange: handleChange
+    }
+}
+
+export default Login;
